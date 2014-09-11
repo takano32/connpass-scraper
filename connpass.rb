d894afdee60ebece46ec07d0903b602106766f76
TAKANO Mitsuhiro <takano32@gmail.com>
2014-09-11 19:12:03 +0900

    first commit

---
+++ connpass.rb
@@ -0,0 +1,49 @@
+require 'nokogiri'
+require 'open-uri'
+
+
+module Connpass
+  CONNPASS_REGEXP = %r!http://connpass\.com/user/([^/]*)/!
+  TWITTER_REGEXP = %r!https://twitter\.com/[^?]*\?screen_name=(.*)!
+  FACEBOOK_REGEXP = %r!https://www\.facebook\.com/(.*)!
+  GITHUB_REGEXP = %r!https://github\.com/(.*)!
+end
+
+class Connpass::Scraper
+  attr_reader :users
+
+  def initialize(event_id)
+    @event_id = event_id
+    url = "http://pyconjp.connpass.com/event/#{@event_id}/participation/"
+    @doc = Nokogiri::HTML(open url)
+    @users = []
+  end
+
+  def scrape
+    @users = []
+    @doc.xpath('//tbody/tr').each do |conpass|
+      conpass.css('td.user').each do |connpass_user|
+        user = {}
+        if Connpass::CONNPASS_REGEXP =~ connpass_user.css('p.display_name a').first[:href] then
+          user[:connpass] = $1
+        end
+        conpass.css('td.social').each do |socials|
+          socials.children.each do |social|
+            social_url = social[:href]
+            if Connpass::TWITTER_REGEXP =~ social_url then
+              user[:twitter] = $1
+            end
+            if Connpass::FACEBOOK_REGEXP =~ social_url then
+              user[:facebook] = $1
+            end
+            if Connpass::GITHUB_REGEXP =~ social_url then
+              user[:github] = $1
+            end
+          end
+        end
+        @users << user
+      end
+    end
+  end
+end
+
---
+++ pyconjp2014.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require_relative './connpass'
+
+s = Connpass::Scraper.new(6300)
+s.scrape
+users = s.users
+
+users.each do |user|
+  if id = user[:twitter] then
+    puts "Twitter: #{id}"
+  end
+end
+
