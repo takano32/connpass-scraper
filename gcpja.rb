d67c0f65c9d70e4b91ca7041c9912f593240194c
TAKANO Mitsuhiro <takano32@gmail.com>
2014-09-18 16:33:41 +0900

    add #gcpja scraper
    http://connpass.com/series/721/

---
+++ gcpja.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require_relative './connpass'
+
+# 7636
+users = [5769, 8120].inject([]) do |users, id|
+  s = Connpass::Scraper.new(id)
+  s.scrape
+  users + s.users
+end.uniq
+
+users.each do |user|
+  if id = user[:twitter] then
+    puts "Twitter: #{id}"
+  end
+end
+
